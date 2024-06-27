266e935c5a22c4b3485c3bd9ff743c3a58981691
Thibaut Chabrier <65714443+Thibaut0710@users.noreply.github.com>
2024-06-27 16:22:02 +0200

    modification fichier tennis1.py

--- python/tennis1.py
+++ python/tennis1.py
@@ -1,49 +1,77 @@
-# -*- coding: utf-8 -*-
-
 class TennisGame1:
+    LOVE = 0
+    FIFTEEN = 1
+    THIRTY = 2
+    FORTY = 3
 
-    def __init__(self, player1Name, player2Name):
+    SCORE_NAMES = {
+        "EN": {
+            0: "Love",
+            1: "Fifteen",
+            2: "Thirty",
+            3: "Forty",
+            "All": "All",
+            "Deuce": "Deuce",
+            "Advantage": "Advantage",
+            "Win": "Win"
+        },
+        "FR": {
+            0: "Zéro",
+            1: "Quinze",
+            2: "Trente",
+            3: "Quarante",
+            "All": "Égalité",
+            "Deuce": "Égalité",
+            "Advantage": "Avantage",
+            "Win": "Victoire"
+        }
+    }
+
+    def __init__(self, player1Name, player2Name, language="FR"):
         self.player1Name = player1Name
         self.player2Name = player2Name
         self.p1points = 0
         self.p2points = 0
+        self.language = language
 
     def won_point(self, playerName):
-        if playerName == "player1":
+        if playerName == self.player1Name:
             self.p1points += 1
-        else:
+        elif playerName == self.player2Name:
             self.p2points += 1
+        else:
+            raise ValueError(f"Unknown player name: {playerName}")
+
+    def _is_tie(self):
+        return self.p1points == self.p2points
+
+    def _is_endgame(self):
+        return self.p1points >= 4 or self.p2points >= 4
+
+    def _get_tie_score(self):
+        if self.p1points in [0, 1, 2]:
+            return f"{self.SCORE_NAMES[self.language][self.p1points]}-{self.SCORE_NAMES[self.language]['All']}"
+        else:
+            return self.SCORE_NAMES[self.language]["Deuce"]
+
+    def _get_endgame_score(self):
+        minusResult = self.p1points - self.p2points
+        if minusResult == 1:
+            return f"{self.SCORE_NAMES[self.language]['Advantage']} {self.player1Name}"
+        elif minusResult == -1:
+            return f"{self.SCORE_NAMES[self.language]['Advantage']} {self.player2Name}"
+        elif minusResult >= 2:
+            return f"{self.SCORE_NAMES[self.language]['Win']} {self.player1Name}"
+        else:
+            return f"{self.SCORE_NAMES[self.language]['Win']} {self.player2Name}"
+
+    def _get_standard_score(self):
+        return f"{self.SCORE_NAMES[self.language][self.p1points]}-{self.SCORE_NAMES[self.language][self.p2points]}"
 
     def score(self):
-        result = ""
-        tempScore=0
-        if (self.p1points==self.p2points):
-            result = {
-                0 : "Love-All",
-                1 : "Fifteen-All",
-                2 : "Thirty-All",
-            }.get(self.p1points, "Deuce")
-        elif (self.p1points>=4 or self.p2points>=4):
-            minusResult = self.p1points-self.p2points
-            if (minusResult==1):
-                result ="Advantage player1"
-            elif (minusResult ==-1):
-                result ="Advantage player2"
-            elif (minusResult>=2):
-                result = "Win for player1"
-            else:
-                result ="Win for player2"
+        if self._is_tie():
+            return self._get_tie_score()
+        elif self._is_endgame():
+            return self._get_endgame_score()
         else:
-            for i in range(1,3):
-                if (i==1):
-                    tempScore = self.p1points
-                else:
-                    result+="-"
-                    tempScore = self.p2points
-                result += {
-                    0 : "Love",
-                    1 : "Fifteen",
-                    2 : "Thirty",
-                    3 : "Forty",
-                }[tempScore]
-        return result
+            return self._get_standard_score()
